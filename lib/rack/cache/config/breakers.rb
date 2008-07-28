53dd082f3a5b06e34dad1b8e058e4628d4329d74
Ryan Tomayko <rtomayko@gmail.com>
2008-07-27 20:58:09 -0700

    fix cache breaker config after validation changes

--- lib/rack/cache/config/breakers.rb
+++ lib/rack/cache/config/breakers.rb
@@ -1,9 +1,9 @@
 # Adds a max-age header to 
 
 on :store do
-  next if @response.freshness_information?
+  next if @object.freshness_information?
   if @request.url =~ /\?\d+$/
     debug 'adding expires headers to cache breaking URL'
-    @response.ttl = 100000000000000
+    @object.ttl = 100000000000000
   end
 end
