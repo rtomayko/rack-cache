d055e6b2b4d61131f40190b63779b7c98dd84437
Ryan Tomayko <rtomayko@gmail.com>
2008-08-09 07:41:59 -0700

    update breakers config after engine refinements

--- lib/rack/cache/config/breakers.rb
+++ lib/rack/cache/config/breakers.rb
@@ -1,9 +1,16 @@
-# Adds a max-age header to 
-
-on :store do
-  next if object.freshness_information?
+# Adds a very long max-age response header when the requested url
+# looks like it includes a cache busting timestamp. Cache busting
+# URLs look like this:
+#   http://HOST/PATH?DIGITS
+#
+# DIGITS is typically the number of seconds since some epoch but
+# this can theoretically be any set of digits. Example:
+#   http://example.com/css/foo.css?7894387283
+#
+on :fetch do
+  next if response.freshness_information?
   if request.url =~ /\?\d+$/
-    debug 'adding expires headers to cache breaking URL'
-    object.ttl = 100000000000000
+    debug 'adding huge max-age to response for cache breaking URL'
+    response.ttl = 100000000000000
   end
 end
