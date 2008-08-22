1d0bda12f7cb6df4406d6d167cc07cac9be1eb69
Ryan Tomayko <rtomayko@gmail.com>
2008-08-22 00:19:14 -0700

    Rack::Cache::EntityStream
    Filter that reads from a Rack response body, writes to a
    destination IOish object, and calls a block when closed.

---
+++ lib/rack/cache/entity_stream.rb
@@ -0,0 +1,68 @@
+module Rack::Cache
+
+  # Stream filter that reads from a source body, writes to a destination IOish
+  # object, and records a SHA1 checksum. An instance of this class meets
+  # Rack's specification for a response body.
+  class EntityStream
+    include Enumerable
+
+    # The Body to read from. Must respond to #each and must only yield String
+    # values. If the Body responds to close, it will be called after
+    # iteration.
+    attr_reader :source
+
+    # An IO-like object that receives data as it passes through the filter.
+    # Should respond to write, puts, flush, and close.
+    attr_reader :dest
+
+    # A Digest::SHA1 object that's fed data as it moves through the filter.
+    attr_reader :digest
+
+    # The number of bytes that have moved through the filter.
+    attr_reader :size
+
+    # Create a new EntityStream that reads from +source+ and writes to +dest+
+    # when the new instance receives an #each. Yield to the block after the
+    # filter has fully processed and closed the streams.
+    def initialize(source, dest, &finish)
+      raise ArgumentError unless source && dest
+      @source, @dest = source, dest
+      @digest = Digest::SHA1.new
+      @size = 0
+      @finish = finish
+    end
+
+    # The SHA1 checksum represented as a 31 character string of hexadecimals.
+    def hexdigest
+      digest.hexdigest
+    end
+
+    # Read a chunk from source, write it to dest, and yield to the block.
+    def each
+      return to_enum(:each) if respond_to?(:to_enum) && !block_given?
+      source.each do |part|
+        @size += part.length
+        dest.write(part)
+        digest << part
+        yield part
+      end
+    end
+
+    # Must be called in order to complete filter processing. Close the
+    # +source+ and +dest+ streams. Yield the EntityStream instance (self)
+    # to the block passed to EntityStream#initialize if present.
+    def close
+      [ source, dest ].each do |receiver|
+        receiver.close if receiver.respond_to? :close
+      end
+      @finish.call self if @finish.respond_to? :call
+    end
+
+    # Perform filter fully (but do not close).
+    def exhaust!
+      each { |part| }
+    end
+
+  end
+
+end
---
+++ test/entity_stream_test.rb
@@ -0,0 +1,82 @@
+require "#{File.dirname(__FILE__)}/spec_setup"
+require 'rack/cache/entity_stream'
+
+describe 'Rack::Cache::EntityStream' do
+
+  before(:each) {
+  }
+
+  after(:each) {
+  }
+
+  class MockIO < StringIO
+    def initialize(*args, &close)
+      super(*args)
+      @close = close
+      @closed = false
+    end
+    def close
+      @closed = true
+      @close.call if @close.respond_to? :call
+      super
+    end
+    def closed?
+      @closed
+    end
+  end
+
+  EntityStream = Rack::Cache::EntityStream
+
+  it 'accepts a source and dest object when created' do
+    stream = EntityStream.new(['hello world'], MockIO.new)
+    %w[write close puts flush].each do |message|
+      stream.dest.should.respond_to message
+    end
+    stream.source.should.be == ['hello world']
+  end
+
+  it 'reads from source and writes to dest when each is invoked with block' do
+    expected = ['hello ', 'world!']
+    finished = false
+    stream = EntityStream.new(expected.dup, MockIO.new)
+    stream.each { |part| part.should.be == expected.shift }
+    stream.dest.string.should.be == 'hello world!'
+  end
+
+  it 'returns an Enumerable when each is invoked without a block' do
+    stream = EntityStream.new(['hello world'], MockIO.new)
+    stream.each.should.respond_to :each
+    called = false
+    stream.each do |part|
+      called = true
+      part.should.be == 'hello world'
+    end
+    called.should.be true
+  end
+
+  it 'calls the finish block when closed' do
+    finished = false
+    EntityStream.new([], MockIO.new) { finished = true }.close
+    finished.should.be true
+  end
+
+  it 'closes dest when closed' do
+    stream = EntityStream.new([], MockIO.new)
+    stream.close
+    stream.dest.should.be.closed
+  end
+
+  it 'closes source when closed when source responds to close' do
+    source, closed = [], false
+    (class<<source;self;end).send(:define_method, :close) { closed = true }
+    EntityStream.new(source, MockIO.new).close
+    closed.should.be == true
+  end
+
+  it 'should generate a SHA hexdigest' do
+    stream = EntityStream.new(['Hello World'], MockIO.new)
+    stream.exhaust!
+    stream.hexdigest.should.be == '0a4d55a8d778e5022fab701977c5d840bbc486d0'
+  end
+
+end
