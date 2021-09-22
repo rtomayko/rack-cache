27b1a7b71c83aa31c6ea4449df4541edcba05b57
Yohei Kitamura <ykitamura@mdsol.com>
2021-09-22 10:03:27 -0400

    Fix case sensitivity of the Vary and Age headers

--- lib/rack/cache/meta_store.rb
+++ lib/rack/cache/meta_store.rb
@@ -34,7 +34,7 @@ module Rack::Cache
 
       # find a cached entry that matches the request.
       env = request.env
-      match = entries.detect{|req,res| requests_match?(res['Vary'], env, req)}
+      match = entries.detect{ |req,res| requests_match?(vary(res), env, req) }
       return nil if match.nil?
 
       _, res = match
@@ -90,13 +90,13 @@ module Rack::Cache
       # the list
       vary = response.vary
       entries =
-        read(key).reject do |env,res|
-          (vary == res['Vary']) &&
+        read(key).reject do |env, res|
+          (vary == vary(res)) &&
             requests_match?(vary, env, stored_env)
         end
 
       headers = persist_response(response)
-      headers.delete 'Age'
+      headers.delete(hash_key(headers, 'Age'))
 
       entries.unshift [stored_env, headers]
       if request.env['rack-cache.use_native_ttl'] && response.fresh?
@@ -163,6 +163,17 @@ module Rack::Cache
         key = "HTTP_#{header.upcase.tr('-', '_')}"
         env1[key] == env2[key]
       end
+    end
+
+    # Retrieve the literal value of the Vary header from a cached response hash
+    # without case sensitivity.
+    def vary(hash)
+      hash[hash_key(hash, 'Vary')]
+    end
+
+    def hash_key(hash, key)
+      hash.each_key { |k| return k if k.casecmp(key).zero? }
+      nil
     end
 
   protected
